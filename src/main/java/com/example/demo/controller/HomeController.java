8341c5aac96b1f2c3cb2266a8cf62f71cca2c735
VAIBHAV GHANEKAR <woh.it.wala@gmail.com>
2023-05-12 20:27:33 +0530

    created two htmls home, about and a base html. created css, js, images folder. configured mysql in properties. created two models for user and contact. created CRUD repo. And a controller, which by now loades the html files.

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.demo.dao.UserRepo;
+import com.example.demo.models.User;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String test(Model model) {
+		model.addAttribute("title", "Home page");
+		return "home.html";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "About page");
+		return "about.html";
+	}
+}
