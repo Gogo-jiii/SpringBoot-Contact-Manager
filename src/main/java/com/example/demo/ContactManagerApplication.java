66bb9507f3c24b5f0af45add96319b7d7944d2c4
VAIBHAV GHANEKAR <woh.it.wala@gmail.com>
2023-05-26 18:18:19 +0530

    added spring security dependency added base packages in the main file

--- src/main/java/com/example/demo/ContactManagerApplication.java
+++ src/main/java/com/example/demo/ContactManagerApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+import com.example.demo.dao.UserRepo;
+
+@SpringBootApplication(scanBasePackages = {"com.example.demo", "com.example.dao", "com.example.controller", "com.example.models"} )
 public class ContactManagerApplication {
 
 	public static void main(String[] args) {
